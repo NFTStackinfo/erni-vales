0fb2e00b85b8e3061eb11987e2d0df6ecc8b839e
Hrach <steepgaskin@gmail.com>
2022-08-15 12:30:55 +0400

    theme-update 2

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -5,14 +5,14 @@ import { Icon } from '../UIKit'
 export const Team = forwardRef(({}, ref) => {
   return (
     <TeamStyle ref={ref}>
-      <Icon name="menu" />
-      <Icon name="arrow-up" />
-      <Icon name="arrow-down" />
+      <Icon name="menu" color="black"/>
+      <Icon name="arrow-up" color="black"/>
+      <Icon name="arrow-down" color="black"/>
       <Icon name="arrow-back" />
       <Icon name="arrow-next" />
-      <Icon name="close" />
-      <Icon name="discord" />
-      <Icon name="twitter" />
+      <Icon name="close" color="black"/>
+      <Icon name="discord" color="black"/>
+      <Icon name="twitter" color="black"/>
     </TeamStyle>
   )
 })
--- src/components/Team/Team.style.js
+++ src/components/Team/Team.style.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const TeamStyle = styled.section`
-
+background-color: ${({ theme }) => theme.colors.darkWhite};
 `;
